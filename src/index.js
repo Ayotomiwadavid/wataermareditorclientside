4fe13b4ac0f3ee75e8353297570138f3c667332a
Ayotomiwadavid <ayotomiwadavid@proton.me>
2025-01-20 01:02:24 +0100

    saving progress

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,5 +10,15 @@ root.render(
   </React.StrictMode>
 );
 
-// Enable service worker
-serviceWorkerRegistration.register();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
